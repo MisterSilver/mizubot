1ad793ae5e88e78cc493c48ef28b9fb0fa102a1c
MisterSilver <39674810+MisterSilver@users.noreply.github.com>
2018-05-28 04:56:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ const FileSync = require('lowdb/adapters/FileSync')
 const client = new Discord.Client();
 bot.login(process.env.TOKEN);
 
-const adapter = new FileSync('database.json');
-const db = low(adapter);
-
-db.defaults({ histoires: [], xp: []}).write()
-
 var prefix = ("/")
 
 
@@ -67,35 +62,6 @@ bot.on('message', message => {
     if (message.content === prefix + "help") {
         message.channel.sendMessage("Liste des commandes: \n -/help");
     }
-
-    //xp
-    var msgauthor = message.author.id;
-
-    if(message.author.bot)return;
-
-    if(!db.get("xp").find({user: msgauthor}).value()){
-        db.get("xp").push({user: msgauthor, xp: 1}).write();
-    }else{
-        var userxpdb = db.get("xp").filter({user: msgauthor}).find('xp').value();
-        console.log(userxpdb);
-        var userxp = Object.values(userxpdb)
-        console.log(userxp)
-        console.log(`Nombre d'xp: ${userxp[1]}`)
-        
-        db.get("xp").findKey({user: msgauthor}).assign({user: msgauthor, xp: userxp[1] += 1}).write();
-
-    if (message.content === prefix + "xp"){
-        var xp = db.get("xp").filter({user: msgauthor}).find('xp').value()
-        var xpfinal = Object.values(xp);
-        var xp_embed = new Discord.RichEmbed()
-            .setTitle(`Stat des XP de ${message.author.username}`)
-            .setColor('#F4D03F')
-            .setDescription("Affichage des XP")
-            .addField("XP:", `${xpfinal[1]} xp`)
-            .setFooter("Mizu")
-        message.channel.send({embed: xp_embed});
-    }
-
     //commande staff pseudo
     if (message.content === prefix + "wonyu"){
         var embed = new Discord.RichEmbed()
